f4fbc568b9b58b34e37f632cfa242c8197696f41
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-05-06 20:39:57 +0200

    Update model.js

--- src/services/posts/model.js
+++ src/services/posts/model.js
@@ -9,11 +9,18 @@ const { Schema, model } = mongoose
 
 const postsSchema = new Schema(
   {
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    email: { type: String, required: true },
-    dateOfBirth: { type: String, required: true },
-    avatar: {type: String, required: false}
+    category:  { type: String, required: true },
+    title: { type: String, required: true } ,
+    cover: { type: String, required: true } ,
+    readTime: {
+      value: { type: Number, required: true },
+      unit: { type: String, required: true }
+ },
+    author: {
+      name: { type: String, required: true },
+      avatar:{ type: String, required: true }
+    },
+    content:{ type: String, required: true }
   },
   {
     timestamps: true, // automatically add createdAt and updatedAt fields
